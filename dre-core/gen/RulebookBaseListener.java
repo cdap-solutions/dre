6e136a7ab4244b6cb29c1f87b35a862f409397b6
Ali <alianwar@google.com>
2019-04-24 00:08:20 -0700

    Fix copyright header end year.

--- dre-core/gen/RulebookBaseListener.java
+++ dre-core/gen/RulebookBaseListener.java
@@ -1,7 +1,7 @@
 // Generated from /Users/nitin/Work/Devel/hydrator/rules-engine/rules-engine-core/src/main/antlr4/co/cask/re/parser/Rulebook.g4 by ANTLR 4.7
 
 /*
- * Copyright © 2017-2018 Cask Data, Inc.
+ * Copyright © 2017-2019 Cask Data, Inc.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
  *  use this file except in compliance with the License. You may obtain a copy of
